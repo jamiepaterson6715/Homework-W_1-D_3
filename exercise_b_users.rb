c120052b65d895c381f814e8c78c8b51fdf870c3
Jamie Paterson <jamiepaterson6715@gmail.com>
2019-05-15 22:34:32 +0100

    W_1/D_3/homework
    cd
    ls
    cd .
    clear
    ~

---
+++ exercise_b_users.rb
@@ -0,0 +1,98 @@
+users = {
+  "Jonathan" => {
+    :twitter => "jonnyt",
+    :lottery_numbers => [6, 12, 49, 33, 45, 20],
+    :home_town => "Stirling",
+    :pets => [
+    {
+      :name => "fluffy",
+      :species => "cat"
+    },
+    {
+      :name => "fido",
+      :species => "dog"
+    },
+    {
+      :name => "spike",
+      :species => "dog"
+    }
+  ]
+  },
+  "Erik" => {
+    :twitter => "eriksf",
+    :lottery_numbers => [18, 34, 8, 11, 24],
+    :home_town => "Linlithgow",
+    :pets => [
+    {
+      :name => "nemo",
+      :species => "fish"
+    },
+    {
+      :name => "kevin",
+      :species => "fish"
+    },
+    {
+      :name => "spike",
+      :species => "dog"
+    },
+    {
+      :name => "rupert",
+      :species => "parrot"
+    }
+  ]
+  },
+  "Avril" => {
+    :twitter => "bridgpally",
+    :lottery_numbers => [12, 14, 33, 38, 9, 25],
+    :home_town => "Dunbar",
+    :pets => [
+      {
+        :name => "monty",
+        :species => "snake"
+      }
+    ]
+  },
+  Jamie =  {
+  :lottery_numbers => [22, 9, 12, 13, 19,27],
+  :home_town => "New Town",
+  :pets => [
+      {
+        :name => "Bracken(d)",
+        :species => "dog"
+      }
+    ]
+  }
+}
+
+# p superhero_avengers[:Hulk][:attacks][:roll]
+
+
+# 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+p users["Jonathan"][:twitter]
+
+# 2. Get Erik's hometown
+users["Erik"][:home_town]
+
+# 3. Get the array of Erik's lottery numbers
+users["Erik"][:lottery_numbers]
+
+# 4. Get the type of Avril's pet Monty
+# users["Avril"][:pets][:name]?????
+
+# 5. Get the smallest of Erik's lottery numbers
+users["Erik"][:lottery_numbers].min
+
+# 6. Return an array of Avril's lottery numbers that are even
+# users["Avril"][:lottery_numbers => [12, 14, 33, 38, 9, 25]]
+# users[:lottery_numbers].each do |x| % 2 == 0 puts X ???
+
+# 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+# fruits << 'lemon'#puts something on end
+users["Erik"][:lottery_numbers] << 7
+
+# 8. Change Erik's hometown to Edinburgh
+users["Erik"][:home_town] = "Edinburgh"
+
+# 9. Add a pet dog to Erik called "Fluffy"
+users["Erik"][:pets] << "fluffy"
+# 10. Add another person to the users hash
